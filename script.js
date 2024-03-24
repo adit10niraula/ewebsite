828addfdb5e3f3c4adee5041bcd78750971b92bd
raj kumar <razzkumar115@gmail.com>
2024-03-24 09:45:01 +0545

    shop and sproduct modified

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Script for navigation bar
+const bar = document.querySelector('#bar')
+const nav = document.querySelector('#navbar')
+const close = document.getElementById('close')
+
+if(bar){
+    bar.addEventListener('click', function(){
+        nav.style.right = "0px";
+    })
+}
+if(close){
+    close.addEventListener('click', function(){
+        nav.style.right="-300px"
+    })
+}
